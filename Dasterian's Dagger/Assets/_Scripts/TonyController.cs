5635f04fc0f2ccca4ae37331570c22db8e8653eb
RithikKalra <71096898+RithikKalra@users.noreply.github.com>
2021-06-03 13:25:10 -0400

    NPC interaction

---
+++ Dasterian's Dagger/Assets/_Scripts/TonyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TonyController : MonoBehaviour
+{
+    private GameObject player;
+    private bool keyPressed = false;
+
+    public GameObject talkText;
+    
+    void Awake()
+    {
+        player = GameObject.Find("Player");
+        talkText.gameObject.SetActive(false);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.transform.position) < 4 && !keyPressed)
+        {
+            talkText.gameObject.SetActive(true);
+            if (Input.GetKeyDown("e"))
+            {
+                keyPressed = true;
+                TonyConvo();
+            }    
+        }
+        else
+        {
+            talkText.gameObject.SetActive(false);
+        }
+    }
+
+    private void TonyConvo()
+    {
+        print("Hello");
+    }
+}
