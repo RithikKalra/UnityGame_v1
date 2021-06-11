51fc219cbdefd546d275e5177812f7de91c2cf99
TaimourArshad <taimour.arshad12@gmail.com>
2021-06-11 00:13:32 -0400

    Bug fixes (check description)
    Player no longer randomly stops in the middle of the game
    Player cannot jump until they touch the ground

--- Dasterian's Dagger/Assets/_Scripts/CharMove2D.cs
+++ Dasterian's Dagger/Assets/_Scripts/CharMove2D.cs
@@ -6,6 +6,8 @@ public class CharMove2D : MonoBehaviour
 {
     public float movementSpeed = 5;
     public float jumpSpeed = 1;
+    bool jumped;
+    private bool isGrounded = true;
 
     private Rigidbody2D _rigidbody;
     private bool push = false;
@@ -20,6 +22,15 @@ public class CharMove2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            jumped = true;
+        }
+       
+    }
+
+    void FixedUpdate()
+    {
         if(!canMove)
         {
             if(_rigidbody.velocity.y > 0.001f || _rigidbody.velocity.y < -0.001f)
@@ -33,28 +44,35 @@ public class CharMove2D : MonoBehaviour
            return;
         }
 
-        if (!push)
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * movementSpeed * Time.deltaTime;
+
+        if (jumped == true && (Mathf.Abs(_rigidbody.velocity.y) < 0.0001f) && isGrounded)
         {
-            var movement = Input.GetAxis("Horizontal");
-            transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * movementSpeed;
-
-            if (Input.GetButtonDown("Jump") && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
-            {
-                _rigidbody.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
-            }
-
-            Vector3 characterScale = transform.localScale;
-            if (Input.GetAxis("Horizontal") < 0)
-            {
-                characterScale.x = 1.2f;
-            }
-            if (Input.GetAxis("Horizontal") > 0)
-            {
-                characterScale.x = -1.2f;
-            }
-            transform.localScale = characterScale;
+            jumped = false;
+            isGrounded = false;
+            _rigidbody.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
         }
-       
+
+        Vector3 characterScale = transform.localScale;
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            characterScale.x = 1.2f;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            characterScale.x = -1.2f;
+        }
+        transform.localScale = characterScale;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D Collision)
+    {
+        if (Collision.gameObject.tag == "Ground" || Collision.gameObject.tag == "Platform" )
+        {
+            isGrounded = true;
+        }
     }
 
     public void basicKnockbackLeft(float strength)
@@ -75,5 +93,5 @@ public class CharMove2D : MonoBehaviour
     private void RemoveControl()
     {
         push = true;
-    }
+    } 
 }
